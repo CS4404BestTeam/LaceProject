faa1a2a88f2dcdbd4435f1b47e3258c0d39bee7a
Evan Duffy <elduffy@wpi.edu>
2018-11-04 14:09:26 -0500

    Added sign and verify functions

--- LaceServer/laceServer.js
+++ LaceServer/laceServer.js
@@ -3,6 +3,10 @@ const db = require("./LaceDatabase");
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser')
+const ursa = require("ursa");
+
+var key = ursa.generatePrivateKey(1024, 65537);
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({
@@ -28,3 +32,19 @@ db.initDatabase(false).then(()=>{
 });
 
 app.listen(3000, () => console.log('Lace open on port 3000!'));
+
+function verify(solecialID, candidate, signature){
+    let data = solecialID.toString() + candidate.toString();
+    var verify = ursa.createVerifier("RSA-SHA256");
+    verify.update(data);
+    let status = verify.verify(key,signature,'hex');
+    return status;
+}
+
+function sign(solecialID, candidate){
+    let data = solecialID.toString() + candidate.toString();
+    var signer = ursa.createSigner("RSA-SHA256");
+    signer.update(data);
+    let signature = signer.sign(key, 'hex');
+    return signature;
+}
